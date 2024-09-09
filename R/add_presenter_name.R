b9404b9ade2af08310e19db55d6d6e71ceb1595f
Candace Savonen <cansav09@gmail.com>
2024-09-09 15:17:07 -0400

    Update to have "VOTE HERE"

--- R/add_presenter_name.R
+++ R/add_presenter_name.R
@@ -11,12 +11,15 @@ add_presenter_name <- function(presenter_name, png) {
 
 image <- png::readPNG(png)
 
-df <-data.frame(presenter_name = presenter_name, x = 0, y =  1)
+df <- data.frame(presenter_name = presenter_name, x = 0, y = 1)
 
-ggplot2::ggplot(df) + 
+
+ggplot2::ggplot(df) +
   ggplot2::annotation_raster(image, xmin = 0, xmax = 1, ymin = 0, ymax = 1) +
-  ggplot2::labs(title = presenter_name) + 
-  ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5))
+  ggplot2::labs(title = "VOTE HERE",
+                subtitle = presenter_name) +
+  ggplot2::theme(plot.title = ggplot2::element_text(color = "red", hjust = 0.5, size = rel(5), face = "bold"), 
+                 plot.subtitle = ggplot2::element_text(hjust = 0.5, size = rel(3)))
 
 ggplot2::ggsave(png)
 
